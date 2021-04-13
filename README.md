c6bc6361eb6a842e1b0c24deea528f8ddb499d00
Piotr Moryto <piotrmoryto@gmail.com>
2021-04-13 14:01:18 +0200

    final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Password Validator
+
+Created to better understand HTML5, CSS && JS basics by example.
+
+### Inspiration
+
+Inspired by JavaScript Course
---
+++ main.js
@@ -0,0 +1,28 @@
+const pass = document.querySelector('#password');
+const p = document.querySelector('.passinfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
+
+const checkPassword = () => {
+    if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)) {
+        p.innerHTML = 'Masz bardzo dobre hasło!';
+        p.style.color = 'lime';
+    } else if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)) {
+        p.innerHTML = 'Masz dobre hasło.';
+        p.style.color = 'orange';
+    } else {
+        p.innerHTML = 'Masz za słabe hasło! :(';
+        p.style.color = 'tomato';
+    }
+}
+
+pass.addEventListener('keyup', function () {
+    if (pass.value !== '') {
+        checkPassword();
+    } else {
+        p.innerHTML = 'Nie podałeś hasła...';
+        p.style.color = '';
+    }
+})
